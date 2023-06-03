0365ce989e1fd6b3b53250648adb1e9eea33fd3e
Dagmara Sendor <dagmara.sendor@gmail.com>
2023-06-03 13:30:01 +0200

    dagmarav3

--- src/app/tabela-umow/tabela-umow.component.ts
+++ src/app/tabela-umow/tabela-umow.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Klient } from '../klient.model';
 import { MOCK_KLIENCI, MOCK_KLIENCI_HEADERS, MOCK_KLIENCI_COLUMNS } from '../mock-klienci';
 
@@ -17,3 +18,41 @@ export class TabelaUmowComponent {
     console.log('Kliknięto klienta:', klient);
   }
 }
+
+@Component({
+  selector: 'app-umowy-table',
+  templateUrl: './umowy-table.component.html',
+  styleUrls: ['./umowy-table.component.css']
+})
+export class UmowyTableComponent {
+  umowaForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.umowaForm = this.formBuilder.group({
+      IDKKlienta: [''],
+      NazwaKlienta: [''],
+      dataOdZawarcia: [''],
+      dataDoZawarcia: [''],
+      czasZawarcia: [''],
+      rodzajUmowy: [''],
+      uwagi: [''],
+      email: [''],
+      systemObslugiZgloszenLink: [''],
+      limitKar: [''],
+      warunkiWypowiedzenia: [''],
+      karaZaWypowiedzenie: [''],
+      koordynatorUmowy: this.formBuilder.group({
+        imie: [''],
+        nazwisko: [''],
+        telefon: [''],
+        email: ['']
+      })
+    });
+  }
+
+  onSubmit() {
+    console.log(this.umowaForm.value);
+    // Przetwarzanie danych, np. wysłanie ich do serwera
+  }
+}
+
